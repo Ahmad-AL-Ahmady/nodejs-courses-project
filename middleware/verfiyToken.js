90f408347a136726761fc0fb41e12b24ae5a418d
Ahmad-AL-Ahmady <ahmad.hussain.alahmady@gmail.com>
2025-01-26 18:52:20 +0200

    refactor: update package.json scripts, improve JWT generation, and enhance user model validation

--- middleware/verfiyToken.js
+++ middleware/verfiyToken.js
@@ -1,26 +1,29 @@
-const jwt = require('jsonwebtoken');
-const httpStatusText = require('../utils/httpStatusText');
-const appError = require('../utils/appError');
+const jwt = require("jsonwebtoken");
+const httpStatusText = require("../utils/httpStatusText");
+const appError = require("../utils/appError");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['Authorization'] || req.headers['authorization'];
-    if(!authHeader) {
-        const error = appError.create('token is required', 401, httpStatusText.ERROR)
-        return next(error);
-    }
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+  if (!authHeader) {
+    const error = appError.create(
+      "Token is required",
+      401,
+      httpStatusText.UNAUTHORIZED
+    );
+    return next(error);
+  }
 
-    const token = authHeader.split(' ')[1];
-    try {
+  const token = authHeader.split(" ")[1];
 
-        const currentUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.currentUser = currentUser;
-        next();
-
-    } catch (err) {
-        const error = appError.create('invalid token', 401, httpStatusText.ERROR)
-        return next(error);
-    }   
-    
-}
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decodedToken:", decodedToken);
+    next();
+  } catch (err) {
+    console.error("Token verification failed:", err);
+    res.status(401).json({ status: "error", message: "Invaled Token" });
+  }
+};
 
 module.exports = verifyToken;
